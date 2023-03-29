f318e1b8fe31db68a97bf2f12224bb4b8ba394af
life is a musical thing <liu.tang.lei@gmail.com>
2023-03-29 11:41:17 +0800

    Update Visvalingam-Whyatt.js

--- Visvalingam-Whyatt.js
+++ Visvalingam-Whyatt.js
@@ -1,34 +1,51 @@
+// Calculate the distance between two points
+const calculationDistance = (point1, point2) => {
+  const ab = calculationDistance(a, b);
+  const bc = calculationDistance(b, c);
+  const ca = calculationDistance(c, a);
+
+  const s = (ab + bc + ca) / 2;
+  const area = Math.sqrt(s * (s - ab) * (s - bc) * (s - ca));
+
+  return area;
+};
+
+// Calculate the area of a triangle
 const calculateTriangleArea = (a, b, c) => {
-    const ab = calculationDistance(a, b);
-    const bc = calculationDistance(b, c);
-    const ca = calculationDistance(c, a);
+  const ab = calculationDistance(a, b);
+  const bc = calculationDistance(b, c);
+  const ca = calculationDistance(c, a);
 
-    const s = (ab + bc + ca) / 2;
-    const area = Math.sqrt(s * (s - ab) * (s - bc) * (s - ca));
+  const s = (ab + bc + ca) / 2;
+  const area = Math.sqrt(s * (s - ab) * (s - bc) * (s - ca));
 
-    return area;
+  return area;
 };
 
 const visvalingamWhyatt = (coordinates, tolerance) => {
-    while (true) {
-        let minArea = Infinity;
-        let minIndex = null;
+  // Continue until only two coordinates are left, or the minimum area is greater than or equal to the tolerance
+  while (coordinates.length > 2) {
+    let minArea = Infinity;
+    let minIndex = null;
 
-        for (let i = 1; i < coordinates.length - 1; i++) {
-            const area = calculateTriangleArea(coordinates[i - 1], coordinates[i], coordinates[i + 1]);
+    // Iterate through each point (excluding the first and last points) and calculate the triangle area
+    for (let i = 1; i < coordinates.length - 1; i++) {
+      const area = calculateTriangleArea(coordinates[i - 1], coordinates[i], coordinates[i + 1]);
 
-            if (area < minArea) {
-                minArea = area;
-                minIndex = i;
-            }
-        }
-
-        if (minArea < tolerance) {
-            coordinates.splice(minIndex, 1);
-        } else {
-            break;
-        }
+      // Update the minimum area and its index if a smaller area is found
+      if (area < minArea) {
+        minArea = area;
+        minIndex = i;
+      }
     }
 
-    return coordinates;
+    // Remove the point with the smallest area if it's less than the tolerance
+    if (minArea < tolerance) {
+      coordinates.splice(minIndex, 1);
+    } else {
+      break;
+    }
+  }
+
+  return coordinates;
 };
