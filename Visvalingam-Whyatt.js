e1adef2960e3a329d779d37e64919060df7d2937
liutanglei <liutanglei@zhiwen.me>
2023-05-16 11:36:51 +0800

    Optimizing Visvalingam-Whyatt algorithm

--- Visvalingam-Whyatt.js
+++ Visvalingam-Whyatt.js
@@ -1,52 +1,81 @@
+// Earth's radius in meters.
 const EARTH_RADIUS = 6370996.81;
-// Calculate the distance between two points
-const calculateDistance = (point1, point2) => {
-  const { latitude: lat1, longitude: lng1 } = point1;
-  const { latitude: lat2, longitude: lng2 } = point2;
-  const radLat1 = lat1 * Math.PI / 180.0;
-  const radLat2 = lat2 * Math.PI / 180.0;
-  const a = radLat1 - radLat2;
-  const b = (lng1 * Math.PI / 180.0) - (lng2 * Math.PI / 180.0);
-  const s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
-  return s * EARTH_RADIUS;
+
+// Helper function to convert degrees to radians.
+const degreeToRadian = function (degree) {
+    return degree * Math.PI / 180.0;
 };
 
-// Calculate the area of a triangle
-const calculateTriangleArea = (a, b, c) => {
-  const ab = calculateDistance(a, b);
-  const bc = calculateDistance(b, c);
-  const ca = calculateDistance(c, a);
+// Function to calculate the distance between two points on the Earth's surface.
+const calculateDistance = function (point1, point2) {
+    // Convert coordinates to radians.
+    const radLat1 = degreeToRadian(point1.latitude);
+    const radLat2 = degreeToRadian(point2.latitude);
+    const radLon1 = degreeToRadian(point1.longitude);
+    const radLon2 = degreeToRadian(point2.longitude);
 
-  const s = (ab + bc + ca) / 2;
-  return Math.sqrt(s * (s - ab) * (s - bc) * (s - ca));
+    // Calculate the differences.
+    const a = radLat1 - radLat2;
+    const b = radLon1 - radLon2;
+
+    // Use the Haversine formula to calculate the great-circle distance.
+    const s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+
+    // Return the distance in meters.
+    return s * EARTH_RADIUS;
 };
 
-const visvalingamWhyatt = (coordinates, tolerance) => {
-  // Continue until only two coordinates are left, or the minimum area is greater than or equal to the tolerance
-  while (coordinates.length > 2) {
-    let minArea = Infinity;
-    let minIndex = null;
+// Function to calculate the area of a triangle formed by three points.
+const calculateTriangleArea = function (a, b, c) {
+    // Calculate the sides of the triangle.
+    const ab = calculateDistance(a, b);
+    const bc = calculateDistance(b, c);
+    const ca = calculateDistance(c, a);
 
-    // Iterate through each point (excluding the first and last points) and calculate the triangle area
-    for (let i = 1; i < coordinates.length - 1; i++) {
-      const area = calculateTriangleArea(coordinates[i - 1], coordinates[i], coordinates[i + 1]);
+    // Use Heron's formula to calculate the area of the triangle.
+    const s = (ab + bc + ca) / 2;
+    return Math.sqrt(s * (s - ab) * (s - bc) * (s - ca));
+};
 
-      // Update the minimum area and its index if a smaller area is found
-      if (area < minArea) {
-        minArea = area;
-        minIndex = i;
-      }
+// Function to perform the Visvalingam-Whyatt simplification on a set of geographic coordinates.
+const visvalingamWhyatt = function (coordinates, tolerance) {
+    // Initialize IndexedCoordinate objects.
+    let indexedCoordinates = coordinates.map((coord, index) => ({
+        ...coord,
+        index,
+    }));
+
+    // Calculate areas for all points except the endpoints.
+    for (let i = 1; i < indexedCoordinates.length - 1; i++) {
+        indexedCoordinates[i].area = calculateTriangleArea(indexedCoordinates[i - 1], indexedCoordinates[i], indexedCoordinates[i + 1]);
     }
 
-    // Remove the point with the smallest area if it's less than the tolerance
-    if (minArea < tolerance) {
-      coordinates.splice(minIndex, 1);
-    } else {
-      break;
-    }
-  }
+    // Iteratively remove the point with the smallest area, until the smallest area exceeds the tolerance.
+    while (indexedCoordinates.length > 2) {
+        // Find the point with the smallest area.
+        const minAreaPoint = indexedCoordinates.slice(1, -1).reduce((prev, curr) => (prev.area < curr.area) ? prev : curr);
 
-  return coordinates;
+        // If the smallest area is larger than the tolerance, stop simplifying.
+        if (minAreaPoint.area >= tolerance) {
+            break;
+        }
+
+        // Remove the point with the smallest area from the coordinates.
+        const index = indexedCoordinates.indexOf(minAreaPoint);
+
+        indexedCoordinates.splice(index, 1);
+        // Update the areas of the two neighboring points.
+        if (index > 1) {
+            indexedCoordinates[index - 1].area = calculateTriangleArea(indexedCoordinates[index - 2], indexedCoordinates[index - 1], indexedCoordinates[index]);
+        }
+
+        if (index < indexedCoordinates.length - 1) {
+            indexedCoordinates[index].area = calculateTriangleArea(indexedCoordinates[index - 1], indexedCoordinates[index], indexedCoordinates[index + 1]);
+        }
+    }
+
+// Return the simplified coordinates in their original order.
+    return indexedCoordinates.sort((a, b) => a.index - b.index);
 };
 
-export default { visvalingamWhyatt }
+export default {visvalingamWhyatt};
