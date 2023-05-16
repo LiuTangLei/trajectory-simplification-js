b47ec2b0b63760e3f4ee08c20832325913e1d4fa
liutanglei <liutanglei@zhiwen.me>
2023-05-16 12:42:54 +0800

    Optimizing Radial-Distance algorithm

--- Radial-Distance.ts
+++ Radial-Distance.ts
@@ -3,41 +3,56 @@ interface Coordinate {
     longitude: number;
 }
 
-const EARTH_RADIUS = 6370996.81;
+const EARTH_RADIUS = 6371000; // Radius of the Earth in meters
 
-// Calculate the distance between two points
+// Convert degree to radian
+const toRadians = (degree: number): number => degree * Math.PI / 180.0;
+
+// Haversine formula to calculate the distance between two points
 const calculateDistance = <T extends Coordinate>(point1: T, point2: T): number => {
     const { latitude: lat1, longitude: lng1 } = point1;
     const { latitude: lat2, longitude: lng2 } = point2;
-    const radLat1 = lat1 * Math.PI / 180.0;
-    const radLat2 = lat2 * Math.PI / 180.0;
-    const a = radLat1 - radLat2;
-    const b = (lng1 * Math.PI / 180.0) - (lng2 * Math.PI / 180.0);
-    const s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
-    return s * EARTH_RADIUS;
+
+    const dLat = toRadians(lat2 - lat1);
+    const dLng = toRadians(lng2 - lng1);
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2)) *
+        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return EARTH_RADIUS * c; // Return distance in meters
 };
 
-// Radial-Distance Algorithm
+/**
+ * Simplifies a given trajectory using the Radial-Distance algorithm.
+ *
+ * @param {Array<T extends Coordinate>} coordinates - The original trajectory coordinates.
+ * @param {number} tolerance - The maximum allowable radial distance (in meters) from a point to the line segment connecting its neighboring points in the simplified trajectory.
+ *
+ * @return {Array<T extends Coordinate>} - The simplified trajectory.
+ *
+ * @throws {Error} If the input coordinates array is null or has fewer than two points.
+ */
 const radialDistance = <T extends Coordinate>(coordinates: T[], tolerance: number): T[] => {
-    // The result array, initialized with the first coordinate
-    const simplifiedCoordinates: T[] = [coordinates[0]];
+    if (!coordinates || coordinates.length < 2) {
+        throw new Error("Invalid input: coordinates array must have at least two points.");
+    }
 
-    // Set the previous point as the first point in the coordinates array
+    const simplifiedCoordinates: T[] = [coordinates[0]]; // Start with the first point
     let previousPoint = coordinates[0];
 
-    // Iterate through the coordinates (excluding the first and last points)
     for (let i = 1; i < coordinates.length - 1; i++) {
-        // Calculate the distance between the current point and the previous point
         const distance = calculateDistance(previousPoint, coordinates[i]);
-
-        // If the distance is greater than or equal to the tolerance, add the current point to the result array and update the previous point
+        // If the distance is greater than or equal to the tolerance, add the point to the result
         if (distance >= tolerance) {
             simplifiedCoordinates.push(coordinates[i]);
-            previousPoint = coordinates[i];
+            previousPoint = coordinates[i]; // Update the previous point
         }
     }
 
-    // Add the last coordinate to the result array
+    // Always include the last point
     simplifiedCoordinates.push(coordinates[coordinates.length - 1]);
 
     return simplifiedCoordinates;
